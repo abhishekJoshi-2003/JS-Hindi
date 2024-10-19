03f9887cd676866eb421a617641d4507d803e258
Abhishek Joshi <joshiabhishek1100@gmail.com>
2024-10-19 12:16:23 +0000

    Variables Learned

---
+++ Basics/variablesAndConstants.js
@@ -0,0 +1,13 @@
+const id = 10001 //This is a constant field which can not be changed in the program
+let name = "Abhishek Joshi" //This is a Good Practise as it avoids the block and functional block scope
+var age = 10; //This is not a Good Approach
+friend = "Nandan" //This is also a way to declare variable but a very bad practise
+let known;
+//id = 1002 This is not allowed
+console.log("Id is" + id)
+console.table([id,name,age,friend,known])
+
+/*
+In such a way variables work
+whenever you do not give value to a Variable then it is considered as undefined in JS
+*/
