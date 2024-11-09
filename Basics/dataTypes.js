add2a34ed83d410fb8092f34695965ebb1ae38e8
Abhishek Joshi <joshiabhishek1100@gmail.com>
2024-11-09 15:01:44 +0000

    Data Types Updated

---
+++ Basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // It would treat the complete code as newer version of JS
+
+// alert("I am Joshi"); // It is used in Browser but can not be used in Node JS
+// Code Readability should be High
+
+let name = "Joshi"
+let age = 22
+let isLoggedIn = false
+let zakir
+
+// Primitive Data Types
+// Number -> Range 2 power 53
+// bigint
+// String -> ""  we can use both double quote as well as single quote
+// Boolean 
+// null -> StandAlone Value / No Value
+// Undefined -> Value not assigned
+// Symbol -> Unique
+
+console.log(typeof "Joshi"); // Gives Type of variable or value
+console.log(typeof undefined); // Its type is Undefined
+console.log(typeof null); // Its Type is Object
