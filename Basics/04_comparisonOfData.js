b4ce5df412e560dfb1b58a8180a0e2dc0dd767d4
Abhishek Joshi <joshiabhishek1100@gmail.com>
2024-11-10 11:28:00 +0000

    Comparison done

---
+++ Basics/04_comparisonOfData.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// All These are simple comparison operators giving boolean values as answers
+
+// In string and number comparison the string is converted as Number
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// NULL Comparison -> To Avoid
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true
+// Null is treated as 0
+// == works deferently than > < >= <=
+
+// Undefined Comparison -> To Avoid
+// console.log(undefined > 0); // false
+// console.log(undefined == 0); // false
+// console.log(undefined >= 0); // false
